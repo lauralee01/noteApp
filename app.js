ea94d3230c41bd2d586229b7cfb8af73ce69c6b8
sajicode <sajioloye@gmail.com>
2018-09-11 15:25:09 +0100

    more functionality

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
+/*console.log(_.isString(true));
+console.log(_.isString('Laura'));*/
+
+var filteredArray = _.uniq(['Laura', 1, 'Joshua', 'Joshua', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+/*var res = notes.addNote();
 console.log(res);
 
-console.log('Result:', notes.add(9, -2));
+console.log('Result:', notes.add(9, -2));*/
 
 /*var user = os.userInfo();
 
