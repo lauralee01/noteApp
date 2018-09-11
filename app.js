06c06981c5d80460d6c0c00db910953eecda1f62
sajicode <sajioloye@gmail.com>
2018-09-11 13:03:25 +0100

    add user info

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 console.log('Starting app.');
+
 const fs = require('fs');
-fs.appendFile('greetings.txt','Hello world!', function(err) {
+const os = require('os');
+
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt',`Hello ${user.username}!`, function(err) {
 	if(err) {
 		console.log('Unable to write to file');
 	}
