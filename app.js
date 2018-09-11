032468412f9d683fed36ab5e428dbaf01ad9cb1f
sajicode <sajioloye@gmail.com>
2018-09-11 13:36:17 +0100

    add notes.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
 
-fs.appendFile('greetings.txt',`Hello ${user.username}!`, function(err) {
+console.log('Result:', notes.add(9, -2));
+
+/*var user = os.userInfo();
+
+fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}.`, function(err) {
 	if(err) {
 		console.log('Unable to write to file');
 	}
-});
+});*/
