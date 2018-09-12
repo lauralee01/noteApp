fdf38a7508c79313f802ec76f682824577ae8990
sajicode <sajioloye@gmail.com>
2018-09-12 13:19:14 +0100

    refactoring code

--- app.js
+++ app.js
@@ -13,7 +13,15 @@ console.log('Yargs', argv);
 
 
 if (command === 'add') {
-	notes.addNote(argv.title, argv.body);
+	var note = notes.addNote(argv.title, argv.body);
+	if (note) {
+		console.log('Note created!');
+		console.log('--');
+		console.log(`Title: ${note.title}`);
+		console.log(`Body: ${note.body}`);
+	} else {
+		console.log('Note taken!');
+	}
 } else if (command === 'list') {
 	notes.getAll();
 } else if (command === 'read') {
