91fe061f29a975a5cb5b2b3dfcd81708b8a16c55
sajicode <sajioloye@gmail.com>
2018-09-12 16:17:37 +0100

    read note

--- app.js
+++ app.js
@@ -16,21 +16,25 @@ if (command === 'add') {
 	var note = notes.addNote(argv.title, argv.body);
 	if (note) {
 		console.log('Note created!');
-		console.log('--');
-		console.log(`Title: ${note.title}`);
-		console.log(`Body: ${note.body}`);
+		notes.logNote(note);
 	} else {
 		console.log('Note taken!');
 	}
 } else if (command === 'list') {
 	notes.getAll();
 } else if (command === 'read') {
-	notes.getNote(argv.title);
+	var note = notes.getNote(argv.title, argv.body);
+	if (note) {
+		console.log('Note avalable!');
+		notes.logNote(note);
+	} else {
+		console.log('Note not availiable');
+	}
 } else if (command === 'remove') {
 	var noteRemoved = notes.removeNote(argv.title);
 	var message = noteRemoved ? 'Note was removed' : 'Note not found'
 	console.log(message);
-	
+
 }else {
 	console.log('Command not recognized');
 }
